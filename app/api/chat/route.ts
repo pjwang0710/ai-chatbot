deacc7f9745fb3ace0d8dd4614a50e04534e3687
pjwang0710 <48118450+pjwang0710@users.noreply.github.com>
2023-08-10 22:21:25 +0800

    Update route.ts

--- app/api/chat/route.ts
+++ app/api/chat/route.ts
@@ -5,10 +5,7 @@ const configuration = new Configuration({
   apiKey: process.env.OPENAI_API_KEY
 })
 
-export const config = {
-  runtime: 'edge',
-};
-
+export const runtime = 'edge'
 const openai = new OpenAIApi(configuration)
 
 export async function POST(req: Request) {
